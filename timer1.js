55d2142bbcbc182be065d61ad4a33c7683e0acd7
JLMaynardDesign <JLMaynardDesign@gmail.com>
2021-12-01 01:41:40 -0500

    commit1

---
+++ timer1.js
@@ -0,0 +1,7 @@
+//Implement an alarm clock / timer which will beep after a specified amount of time has passed. The user can specify an unlimited number of alarms using command line arguments
+
+//retrieve commandline input on the number of alarms
+//the input will be sorted into a new array according to ascending orders
+// code for beep sound: process.stdout.write('\x07');
+//utilize if statements -> (1) no beep for no number, (2) ignore negative number (when sorting the array) and ignore non-number input
+
